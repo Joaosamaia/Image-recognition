acbbf48bf4996dc82233aac2b7ebfcaca0b59761
Amanda Oliveira <143821283+nandaoli22@users.noreply.github.com>
2024-06-11 19:25:34 -0300

    Updating app.py modulation

---
+++ image_recognition/__init__.py
@@ -0,0 +1,1 @@
+from .app import create_initial_window
--- setup.py
+++ setup.py
@@ -8,14 +8,12 @@ setup(
         'opencv-python-headless>=4.5.3.56',
         'numpy>=1.21.1',
         'Pillow>=8.3.1',
-        'matplotlib>=3.4.3',
-        'jupyter>=1.0.0',
     ],
     author='João Lucas S. A. Maia',
     description='A computer desktop app for object recognition and classification using Python.',
     entry_points={
         'console_scripts': [
-            'image-detection=app:create_initial_window',
+            'image-detection=image_recognition.app:create_initial_window',
         ],
     },
 )
